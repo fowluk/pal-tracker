0bd4e65c389de2a369e9c47c195f998dfc72bb78
James Jeffrey <jjeffrey@pivotal.io>
2019-06-12 15:18:15 +0100

    cloud native database access

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,15 +1,24 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     public TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository(this.dataSource);
     }
 
     public static void main(String[] args) {
--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -4,12 +4,8 @@ import java.util.List;
 
 public interface TimeEntryRepository {
     TimeEntry create(TimeEntry any);
-
     TimeEntry find(long timeEntryId);
-
     List<TimeEntry> list();
-
     TimeEntry update(long eq, TimeEntry any);
-
     void delete(long timeEntryId);
 }
